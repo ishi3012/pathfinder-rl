0a6008c8cc4ae1bec0096cc0d4d301d9c3d6ba25
Shilpa Musale <shilpa.musale02@gmail.com>
2025-02-01 18:39:17 +0000

    Fix: Added Difficulty levels

--- TicTacToe-DQN-AI/src/game_logic.py
+++ TicTacToe-DQN-AI/src/game_logic.py
@@ -46,7 +46,12 @@ class TicTacToeAI:
             pickle.dump(self.q_table, f)
 
     def set_difficulty(self, difficulty):
-        self.epsilon = difficulty  # Mostly optimal moves
+        if difficulty == 'Easy':
+            self.epsilon = 0.5  # More random moves
+        elif difficulty == 'Medium':
+            self.epsilon = 0.2  # Balanced exploration
+        elif difficulty == 'Hard':
+            self.epsilon = 0.1  # Mostly optimal moves
 
     def load_q_table(self):
         if os.path.exists("q_table.pkl"):
@@ -64,8 +69,8 @@ st.markdown("""
             background-color: #f5f5f5; color: #333;
         }
         .stButton>button {
-            background-color: #ff9800;
-            color: white;
+            background-color: #e09f36;
+            color: #0a0a0a;
             font-size: 18px;
             border-radius: 10px;
             padding: 10px;
@@ -73,12 +78,12 @@ st.markdown("""
             transition: 0.3s;
         }
         .stButton>button:hover {
-            background-color: #e68900;
+            background-color: #068f23;
         }
         .win-message {
             font-size: 24px;
             font-weight: bold;
-            color: #FFD700;
+            color: #05991d;
             text-align: center;
         }
         .tic-tac-toe-grid button {
